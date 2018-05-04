0b9e221c9822a7a2d1a8fd2af00b6cc73c24babc
Eduard Dudar <eduard.dudar@oath.com>
2018-05-04 11:50:07 -0700

    Initial commit

---
+++ configurer/src/main/java/com/github/edudar/sbe/configurer/ConfApp.java
@@ -0,0 +1,102 @@
+package com.github.edudar.sbe.configurer;
+
+import org.jasypt.util.text.BasicTextEncryptor;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.core.env.ConfigurablePropertyResolver;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class ConfApp implements CommandLineRunner {
+
+    @Component
+    public static class Service {
+
+        @Value("${application.name}")
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    @Component
+    @ConfigurationProperties(prefix = "application")
+    public static class Application {
+
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+    @Component
+    @ConfigurationPropertiesBinding
+    public static class EncryptionConverter implements Converter<String, String> {
+
+        private final BasicTextEncryptor encryptor;
+
+        public EncryptionConverter() {
+            this.encryptor = new BasicTextEncryptor();
+            this.encryptor.setPassword("test");
+        }
+
+        @Override
+        public String convert(String source) {
+            return source.startsWith("ENC:") ? this.encryptor.decrypt(source.substring(4)) : source;
+        }
+    }
+
+    @Component
+    public static class EncryptionAwarePropertySourcesPlaceholderConfigurer extends PropertySourcesPlaceholderConfigurer {
+
+        private final BasicTextEncryptor encryptor;
+
+        public EncryptionAwarePropertySourcesPlaceholderConfigurer() {
+            this.encryptor = new BasicTextEncryptor();
+            this.encryptor.setPassword("test");
+        }
+
+        @Override
+        protected void processProperties(ConfigurableListableBeanFactory beanFactoryToProcess, ConfigurablePropertyResolver propertyResolver) throws BeansException {
+            super.processProperties(beanFactoryToProcess, propertyResolver);
+            doProcessProperties(beanFactoryToProcess,
+                    value -> value.startsWith("ENC:") ? this.encryptor.decrypt(value.substring(4)) : value);
+        }
+    }
+
+    @Autowired
+    Service service;
+
+    @Autowired
+    Application application;
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder()
+                .sources(ConfApp.class)
+                .web(WebApplicationType.NONE)
+                .build()
+                .run(args);
+    }
+
+    @Override
+    public void run(String... args) {
+        System.out.println(service.getName());
+        System.out.println(application.getName());
+    }
+}
---
+++ environment/src/main/java/com/github/edudar/sbe/environment/EnvApp.java
@@ -0,0 +1,126 @@
+package com.github.edudar.sbe.environment;
+
+import org.jasypt.util.text.BasicTextEncryptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.core.env.StandardEnvironment;
+import org.springframework.stereotype.Component;
+
+@SpringBootApplication
+public class EnvApp implements CommandLineRunner {
+
+    @Component
+    public static class Service {
+
+        @Value("${application.name}")
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+    }
+
+    @Component
+    @ConfigurationProperties(prefix = "application")
+    public static class Application {
+
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
+    @Component
+    @ConfigurationPropertiesBinding
+    public static class EncryptionConverter implements Converter<String, String> {
+
+        private final BasicTextEncryptor encryptor;
+
+        public EncryptionConverter() {
+            this.encryptor = new BasicTextEncryptor();
+            this.encryptor.setPassword("test");
+        }
+
+        @Override
+        public String convert(String source) {
+            return source.startsWith("ENC:") ? this.encryptor.decrypt(source.substring(4)) : source;
+        }
+    }
+
+    public static class EncryptionAwareStandardEnvironment extends StandardEnvironment {
+
+        private final BasicTextEncryptor encryptor;
+
+        public EncryptionAwareStandardEnvironment() {
+            this.encryptor = new BasicTextEncryptor();
+            this.encryptor.setPassword("test");
+        }
+
+        @Override
+        public String getProperty(String key) {
+            return decrypt(super.getProperty(key));
+        }
+
+        @Override
+        public String getProperty(String key, String defaultValue) {
+            return decrypt(super.getProperty(key, defaultValue));
+        }
+
+        @Override
+        public <T> T getProperty(String key, Class<T> targetType) {
+            return super.getProperty(key, targetType);
+        }
+
+        @Override
+        public <T> T getProperty(String key, Class<T> targetType, T defaultValue) {
+            return super.getProperty(key, targetType, defaultValue);
+        }
+
+        @Override
+        public String getRequiredProperty(String key) throws IllegalStateException {
+            return decrypt(super.getRequiredProperty(key));
+        }
+
+        @Override
+        public <T> T getRequiredProperty(String key, Class<T> targetType) throws IllegalStateException {
+            return super.getRequiredProperty(key, targetType);
+        }
+
+        private String decrypt(String value) {
+            return value != null && value.startsWith("ENC:") ? this.encryptor.decrypt(value.substring(4)) : value;
+        }
+    }
+
+    @Autowired
+    Service service;
+
+    @Autowired
+    Application application;
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder()
+                .sources(EnvApp.class)
+                .environment(new EncryptionAwareStandardEnvironment())
+                .web(WebApplicationType.NONE)
+                .build()
+                .run(args);
+    }
+
+    @Override
+    public void run(String... args) {
+        System.out.println(service.getName());
+        System.out.println(application.getName());
+    }
+}
